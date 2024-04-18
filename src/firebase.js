374c4b9b7a3f53a39d130a3a47f0aaa0f4348885
juhaina1996 <juhaina.mt@rariti.io>
2024-04-18 11:12:40 +0530

    Added firebase config to env file

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,8 @@ import { getStorage } from "firebase/storage";
 
 import { GoogleAuthProvider, getAuth, signOut } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-const firebaseConfig = {
-  apiKey: "AIzaSyA1U8365CTUDqshzPTTSreNREkViMO3Xkk",
-  authDomain: "payezy-app-d3112.firebaseapp.com",
-  projectId: "payezy-app-d3112",
-  storageBucket: "payezy-app-d3112.appspot.com",
-  messagingSenderId: "731185304637",
-  appId: "1:731185304637:web:5e9db6234129c4639a0654",
-  measurementId: "G-6N5DPGBJRD",
-};
 
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
